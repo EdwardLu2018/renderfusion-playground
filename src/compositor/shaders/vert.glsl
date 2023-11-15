7bed999fb8329fa5e5dcdc944385823c13eb682b
Edward <lued2018@gmail.com>
2023-11-15 18:40:39 -0500

    upgrade aframe version

--- src/compositor/shaders/vert.glsl
+++ src/compositor/shaders/vert.glsl
@@ -1,8 +1,5 @@
 varying vec2 vUv;
 
-uniform mat4 cameraLProjectionMatrix, cameraLMatrixWorld;
-uniform mat4 cameraRProjectionMatrix, cameraRMatrixWorld;
-
 void main() {
     vUv = uv;
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
